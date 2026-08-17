777a03e95814ba0a1728449a7827ef75025e218f
eliscare <infomomtelo@gmail.com>
2026-08-17 10:18:44 -0700

    Set base path to './' for asset compatibility
    Set base path for asset files to ensure compatibility with Cloudflare.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Forces relative file asset paths so Cloudflare reads the bundle perfectly
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false
