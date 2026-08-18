f29d915f8310181821b3bed89084396c3690290c
eliscare <infomomtelo@gmail.com>
2026-08-17 18:09:52 -0700

    Create coursesData.ts with Course interface and data
    Added an interface for Course and a constant array of course data.

---
+++ src/coursesData.ts
@@ -0,0 +1,22 @@
+export interface Course {
+  id: number;
+  title: string;
+  hours: string;
+}
+
+export const COURSES_DATA: Course[] = [
+  { "id": 1, "title": "Dementia Care: This Is Your Brain on Music", "hours": "1 hour" },
+  { "id": 2, "title": "Dementia-Related Behaviors", "hours": "3 hours" },
+  { "id": 3, "title": "Dementia: Causes, Symptoms and Types", "hours": "2 hours" },
+  { "id": 4, "title": "Dementia: Getting Through the Day", "hours": "2 hours" },
+  { "id": 5, "title": "Hospice Care for RCFE", "hours": "2 hours" },
+  { "id": 6, "title": "Infection Control", "hours": "1 hour" },
+  { "id": 7, "title": "Managing Aggressive Behaviors", "hours": "0.5 hour" },
+  { "id": 8, "title": "Osteoporosis", "hours": "2 hours" },
+  { "id": 9, "title": "Physical Changes in Aging", "hours": "1 hour" },
+  { "id": 10, "title": "Postural Supports in RCFE", "hours": "2 hours" },
+  { "id": 11, "title": "Psychosocial Needs of Elders", "hours": "2 hours" },
+  { "id": 12, "title": "Recognizing and Reporting Abuse", "hours": "1 hour" },
+  { "id": 13, "title": "Residents' Rights in RCFE", "hours": "1 hour" },
+  { "id": 14, "title": "Restricted and Prohibited Conditions RCFE", "hours": "1 hour" }
+];
